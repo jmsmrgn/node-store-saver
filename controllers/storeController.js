4f39763f3fd8a2ce4252aa7f964f46274bdb7583
jmsmrgn <hello@jmsmrgn.com>
2017-06-05 15:10:12 -0700

    Add store editing

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -20,3 +20,23 @@ exports.getStores = async (req, res) => {
   const stores = await Store.find()
   res.render('stores', { title: 'Stores', stores })
 }
+
+exports.editStore = async (req, res) => {
+  // 1. find store given the ID
+  const store = await Store.findOne({ _id: req.params.id })
+  // 2. confirm they're the owner of the store
+  // TODO
+  // 3. render edit form so user can update their store
+  res.render('editStore', { title: `Edit ${store.name}`, store })
+}
+
+exports.updateStore = async (req, res) => {
+  // 1. find and update store
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true, // return new store instead of old one
+    runValidators: true
+  }).exec()
+  req.flash('success', `Succesfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store ➡</a>`)
+  // 2. redirect them to store and tell them it worked
+  res.redirect(`/stores/${store._id}/edit`)
+}
