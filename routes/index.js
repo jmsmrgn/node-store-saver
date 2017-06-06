e5c62b158db69499cee715f04bdcbc66629ea98b
jmsmrgn <hello@jmsmrgn.com>
2017-06-06 16:23:02 -0700

    Create tags route and template

--- routes/index.js
+++ routes/index.js
@@ -19,9 +19,10 @@ router.post('/add/:id',
   catchErrors(storeController.updateStore)
 )
 
-// show single store
+router.get('/stores/:id/edit', catchErrors(storeController.editStore))
 router.get('/store/:slug', catchErrors(storeController.getStoreBySlug))
 
-router.get('/stores/:id/edit', catchErrors(storeController.editStore))
+router.get('/tags', catchErrors(storeController.getStoresByTag))
+router.get('/tags/:tag', catchErrors(storeController.getStoresByTag))
 
 module.exports = router
