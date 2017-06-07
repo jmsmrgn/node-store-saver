0e0cea4eee490506c044f9b8c41320cff09600b0
jmsmrgn <hello@jmsmrgn.com>
2017-06-07 16:23:58 -0700

    Set up user permissions

--- models/Store.js
+++ models/Store.js
@@ -33,7 +33,12 @@ const storeSchema = new Schema({
       required: 'You must supply an address!!'
     }
   },
-  photo: String
+  photo: String,
+  author: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: 'You must supply an author'
+  }
 })
 
 storeSchema.pre('save', async function(next) {
