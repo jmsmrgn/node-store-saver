e90867c0d6cbc26deb540fc1b64e3a59c8c504a6
jmsmrgn <hello@jmsmrgn.com>
2017-06-13 21:54:53 -0700

    Add pagination

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -51,9 +51,27 @@ exports.createStore = async (req, res) => {
 }
 
 exports.getStores = async (req, res) => {
-  // 1. query db for list of all stores
-  const stores = await Store.find()
-  res.render('stores', { title: 'Stores', stores })
+  const page = req.params.page || 1
+  const limit = 4
+  const skip = (page * limit) - limit
+
+  // query db for list of all stores
+  const storesPromise = Store
+    .find()
+    .skip(skip)
+    .limit(limit)
+    .sort({ created: 'desc' })
+
+  const countPromise = Store.count()
+
+  const [stores, count] = await Promise.all([storesPromise, countPromise])
+  const pages = Math.ceil(count / limit)
+  if (!stores.length && skip) {
+    req.flash('info', `Hey! You asked for ${page}. But that doesn't exist. So I put you on page ${pages}`)
+    res.redirect(`/stores/page/${pages}`)
+    return
+  }
+  res.render('stores', { title: 'Stores', stores, page, pages, count })
 }
 
 const confirmOwner = (store, user) => {
